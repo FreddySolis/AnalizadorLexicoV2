1a5859305b23cd98b01ce34a42212369f893a72a
Freddy <173239@ids.upchiapas.edu.mx>
2020-02-03 17:39:19 -0600

    Primer intento

---
+++ src/analizador/lexico/v2/Palabras.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizador.lexico.v2;
+
+/**
+ *
+ * @author Freddy
+ */
+public class Palabras {
+    private String palabra;
+    private String tipo;
+    
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
